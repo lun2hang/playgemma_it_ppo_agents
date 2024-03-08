88c12a6b8f508952240a38e7ea5b67440d5e35d9
lun2hang <wind_lunzhang@hotmail.com>
2024-03-08 15:46:00 +0800

    sft and rl are interleaved

--- gemma2b_it_joketeller_ppo.py
+++ gemma2b_it_joketeller_ppo.py
@@ -1,7 +1,6 @@
-#init gemma7b as reward model to score the joke 
+#init gemma7b as reward model to score a joke 
 
-#init A Bmodel as PEFT mode for rl
-
+#init both A B model as PEFT mode for rl
 
 #outer loop
 
@@ -9,18 +8,20 @@
 
 #load all models seperatly
 
-#all models generate a joke seperatly,using a random seed
+#all models generate a joke seperatly,using a different random seed
 
 #reward model give all the jokes a score seperatly
 
-#both model AB run a ppo step,and save the generation if reward is bigger than threshhold
+#both model A B run a PPO step,and save the generation as positive sample if reward is bigger than threshhold
 
 #inner rl loop end
 
 #model save checkpoint
-#eval
+#evaluate with reward model
 
 #A model sfted by positive sample from B, vice versa
 
 #model save checkpoint
-#eval
+#eval with reward model
+
+#end outer loop
